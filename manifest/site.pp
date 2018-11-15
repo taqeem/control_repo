c48e0fdfc7030bf829461033e4c6d136783176e1
Mir Taqee <mir.taqeeulla@dell.com>
2018-11-16 00:49:34 +0530

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,8 +1,4 @@
 node default {
-  file { '/root/README':
-    ensure => file,
-    content => 'This is a readme',
-  }
   file { '/root/test':
     ensure => file,
     content => 'This is a readme',
