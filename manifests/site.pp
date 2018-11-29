fa3db81b6f1b7810d60ffc601c3ff0a8203451ed
Mir Taqee <mir.taqeeulla@dell.com>
2018-11-29 08:47:57 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -20,5 +20,15 @@ node default {
   notify {'HelloWorld':}
 }
 
+node 'master.puppet.vm' {
+  include role::master_server
+}
+
+node /^web/ { 
+  include role::app_server
+}
+node /^db/ {
+  include role::db_server
+}
 
 
