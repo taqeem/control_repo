cf3c7c48c13155e9d8b95941369edeae080fe589
Mir Taqee <mir.taqeeulla@dell.com>
2018-11-29 08:42:43 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,27 +7,9 @@ node default {
     ensure => file,
     owner => 'root',
   } 
-  file { '/root/READMEDir':
-    ensure => directory,
-    owner => 'root',
-    content => 'test....'
-  } 
-  user { 'mir': 
-    ensure => present, 
-    home   => '/home/mir', 
-    #manage_home => true,
-  }
+
   notify {'HelloWorld':}
 }
 
 
-node 'master.puppet.vm' {
-  include role::master_server
-}
 
-node /^web/ { 
-  include role::app_server
-}
-node /^db/ {
-  include role::db_server
-}
